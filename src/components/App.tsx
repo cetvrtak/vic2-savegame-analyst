0ee1a7dfc2c4f8e0cb5b0b8ad83c21c61f24ac72
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-07-31 07:17:27 +0200

    Load save

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import LoadSave from './LoadSave';
+import './App.css';
+
+const App: React.FC = () => {
+  return <LoadSave />;
+};
+
+export default App;
---
+++ src/components/LoadSave.tsx
@@ -0,0 +1,51 @@
+﻿import React, { useRef, useState } from 'react';
+import { parseFileStream, ParsedObject } from './parser';
+
+const LoadSave: React.FC = () => {
+  const [fileContent, setFileContent] = useState<ParsedObject | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFile = async (file: File) => {
+    const stream = file.stream();
+    const parsedData = await parseFileStream(stream);
+    setFileContent(parsedData);
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      handleFile(file);
+    }
+  };
+
+  return (
+    <div>
+      <div className="file-input-wrapper">
+        <button className="custom-file-button" onClick={handleClick}>
+          Load save
+        </button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          className="file-input"
+          onChange={handleFileChange}
+        />
+      </div>
+
+      {fileContent && (
+        <div className="row">
+          <span>{fileContent.player}</span>
+          <span>{fileContent.RUS.import}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoadSave;
