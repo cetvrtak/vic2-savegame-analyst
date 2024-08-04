10197b747d2865d67d57290a06e45d4c283ad126
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-08-04 11:36:41 +0200

    Use reducer

---
+++ src/components/reducer.tsx
@@ -0,0 +1,14 @@
+﻿import { AppState } from './types';
+import { Action } from './actions';
+
+export const reducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    case 'SET_WORLD':
+      return {
+        ...state,
+        world: action.payload,
+      };
+    default:
+      return state;
+  }
+};
