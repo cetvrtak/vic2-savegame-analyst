c7f6500388b06141bc68e171bedb1c80b93e2743
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-11-25 22:48:53 +0100

    Enable merging objects in JSON Exporter - no file name keys

--- src/tools/JsonExporter.tsx
+++ src/tools/JsonExporter.tsx
@@ -1,32 +1,44 @@
-﻿import React, { useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { parseFileStream } from '../components/parser';
 
 const JsonExporter: React.FC = () => {
-  const [fileName, setFileName] = useState<string | null>(null);
+  const [fileName, setFileName] = useState<string>('');
   const [jsonResult, setJsonResult] = useState<string | null>(null);
+  const [files, setFiles] = useState<FileList | null>(null);
+  const [mergeObjects, setMergeObjects] = useState<Boolean>(false);
 
   const handleFileChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const files = event.target.files;
-    if (files && files.length > 0) {
-      try {
-        let results: Record<string, any> = {};
-        for (const file of Array.from(files)) {
-          const fileNameWithoutExt = file.name.split('.')[0];
-          const parsedResult = await parseFileStream(file.stream(), () => {});
-          setFileName(fileNameWithoutExt);
-          if (files.length === 1) {
-            results = parsedResult;
-          } else {
-            results[fileNameWithoutExt] = parsedResult;
+    setFiles(event.target.files);
+  };
+
+  useEffect(() => {
+    const generateJson = async () => {
+      if (files && files.length > 0) {
+        try {
+          let results: Record<string, any> = {};
+          for (const file of Array.from(files)) {
+            const fileNameWithoutExt = file.name.split('.')[0];
+            const parsedResult = await parseFileStream(file.stream(), () => {});
+            setFileName(fileNameWithoutExt);
+            if (files.length === 1 || mergeObjects) {
+              results = { ...results, ...parsedResult };
+            } else {
+              results[fileNameWithoutExt] = parsedResult;
+            }
           }
+          setJsonResult(JSON.stringify(results, null, 2));
+        } catch (error) {
+          console.error('Error parsing file(s):', error);
         }
-        setJsonResult(JSON.stringify(results, null, 2));
-      } catch (error) {
-        console.error('Error parsing file(s):', error);
       }
-    }
+    };
+    generateJson();
+  }, [files, mergeObjects]);
+
+  const handleCheckboxToggle = () => {
+    setMergeObjects((merge) => !merge);
   };
 
   const downloadJson = () => {
@@ -50,6 +62,12 @@ const JsonExporter: React.FC = () => {
         onChange={handleFileChange}
         multiple
       />
+      {files && files.length > 1 && (
+        <label className="tool-box-checkbox" onClick={handleCheckboxToggle}>
+          <input type="checkbox" />
+          <span>Merge objects</span>
+        </label>
+      )}
       {jsonResult && (
         <div>
           <button className="btn" onClick={downloadJson}>
