d10e72636adfade788182ca5e8c2e3d359c941dd
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2026-04-03 22:23:08 +0200

    Use CSS module for ToolsIcon component

--- src/tools/ToolsIcon.tsx
+++ src/tools/ToolsIcon.tsx
@@ -1,10 +1,12 @@
+import styles from './ToolsIcon.module.css';
+
 const ToolsIcon: React.FC<{
   toggleTools: () => void;
 }> = ({ toggleTools }) => {
   return (
     <div>
-      <div className="tools-icon-container" onClick={() => toggleTools()}>
-        <img src="tools.svg" alt="Tools Icon" className="tools-icon" />
+      <div className={styles.toolsIconContainer} onClick={() => toggleTools()}>
+        <img src="tools.svg" alt="Tools Icon" className={styles.toolsIcon} />
       </div>
     </div>
   );
